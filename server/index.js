91e8665ad2cc6402054a942a32c9cdb669af9767
Mritanjay kr harijan <mritunjaykr9933@gmail.com>
2022-07-03 10:27:16 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,33 @@
-import  express  from "express";
+import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
+
+//const CONNECTION_URL ="mongodb+srv://mritunjay:Sunnykr9932@cluster0.wi72g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//mongoose.set("useFindAndModifi", false);
---
+++ server/routes/posts.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import {
+  getPostBySearch,
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  getPost,
+  commentPost,
+} from "../controllers/posts.js";
+
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/search", getPostBySearch);
+router.get("/", getPosts);
+router.get("/:id",getPost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", auth, commentPost);
+
+export default router;
